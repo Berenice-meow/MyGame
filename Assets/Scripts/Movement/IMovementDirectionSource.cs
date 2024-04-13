470453317062c4424ba054d18da1b96ffd15d909
Berenice-meow <berenicemeow16@gmail.com>
2024-04-13 11:39:24 +0300

    Commit 1

---
+++ Assets/Scripts/Movement/IMovementDirectionSource.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace MyGame.Movement
+{
+    public interface IMovementDirectionSource   //Получаем направление движения через Интерфейс, чтобы м/б разные Source (устройства ввода): клавамышь, экран, джойстик
+    {
+        Vector3 MovementDirection { get; }
+    }
+}
